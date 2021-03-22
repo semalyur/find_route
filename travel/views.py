0d6f90924559b86cc1bc66586a19ec46a6fea6fa
semalyur <semalyur200411@gmail.com>
2021-03-22 22:33:00 +0300

    03  07

--- travel/views.py
+++ travel/views.py
@@ -6,3 +6,8 @@ def home(request):
     name = 'Bob'
 
     return render(request, 'home.html', {'name': name})
+
+def about(request):
+    name = 'About us'
+
+    return render(request, 'about.html', {'name': name})
